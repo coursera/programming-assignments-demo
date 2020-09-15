aff94e715ea699226945164818062ee27c317180
Sashi Penta <sashi@coursera.org>
2020-09-15 15:42:19 -0700

    [WIP] Java GridV2 Format grader with environment variables

---
+++ custom-graders/v2/DemoJavaGrader/Grader.java
@@ -0,0 +1,124 @@
+// Input 1: Name of file containing solutions
+// Input 2: Name of the file where output from learner's program is  stored.
+
+// `Grader` compares the two files and prints 'fractionalScore' and 'feedback' based on whether the two files match or not.
+
+// example: If files match exactly, the output is - {"fractionalScore": 1.0, "feedback": "Congrats! All test cases passed!"}
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import java.util.Map;
+
+public class Grader {
+	public static void main(String[] args) {
+		// Point penalty for each failing test case. This means if more than 5 test cases fail,
+		// learner will get 0.0 as his score.
+		double testCasePenalty = 0.2;
+
+		// Number of test cases failed.
+		int numTestCasesFailed = 0;
+
+		// Final fractional score
+		double finalFractionalScore = 0.0;
+
+		Map<String, String> env = System.getenv();
+
+		// Print Evnivornment variables
+		System.out.println("Environment variables: \n");
+        	for (String envName : env.keySet()) {
+            		System.out.format("%s=%s%n",
+                              envName,
+                              env.get(envName));
+        	}
+
+		boolean local = env.get("LOCAL").equals("1");
+
+		String feedbackfile = "/shared/feedback.json";
+		String richFeedbackFile = "richFeedback.html";
+
+		if(local) {
+			feedbackfile = "./shared/feedback.json";
+		}
+
+		String feedback;
+		System.err.println("cwd: " + System.getProperty("user.dir"));
+		try {
+			BufferedReader assignmentSolution = new BufferedReader(new FileReader(args[0]));
+			BufferedReader learnerSolution = new BufferedReader(new FileReader(args[1]));
+
+			String input;
+			String output;
+
+			for (input = assignmentSolution.readLine(), output = learnerSolution.readLine();
+				input != null && output != null;
+				input = assignmentSolution.readLine(), output = learnerSolution.readLine()) {
+				if (!input.equals(output)) {
+					numTestCasesFailed++;
+				}
+			}
+
+			if (input != null || output != null) {
+				finalFractionalScore = 0.0;
+				feedback = "Invalid output. The number of lines produced by your code are not valid.";
+			} else if (numTestCasesFailed > 0) {
+				// We're penalizing testCasePenalty for each test case failed.
+				double totalPenalty = Math.min(1.0, (testCasePenalty * numTestCasesFailed));
+
+				finalFractionalScore = 1.0 - totalPenalty;
+				feedback = "Your solution failed " + numTestCasesFailed + " test cases. Please try again!";
+			} else {
+				finalFractionalScore = 1.0;
+				feedback = "Congrats! All test cases passed!";
+			}
+
+			assignmentSolution.close();
+			learnerSolution.close();
+
+		} catch(IOException io) {
+			System.err.println("Got an exception!");
+			System.err.println(io.getMessage());
+			io.printStackTrace(System.err);
+			feedback = io.getMessage();
+		}
+
+	
+		// Construct jsonOutput in the format expected by Coursera's infrastructure.
+
+		// fractionalScore must be between 0.0 to 1.0.
+		String feedbackStdout = feedback + " (GrID V2 stdout)";
+		String jsonOutput = "{" + "\"fractionalScore\":" + finalFractionalScore + "," + "\"feedback\":" + "\"" + feedbackStdout + "\"" + "}";
+		System.out.println(jsonOutput);
+
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(feedbackfile));
+			String feedbackUpdated = feedback + " (GrID V2 feedback)";
+			jsonOutput = "{" + 
+				"\"fractionalScore\":" + finalFractionalScore + "," + 
+				"\"feedback\":" + "\"" + feedbackUpdated + "\"" + "," +
+				"\"richFeedbackFile\":" + "\"" + richFeedbackFile + "\"" +
+			    "}";
+			writer.write(jsonOutput);
+			writer.close();	
+
+			String richFeedbackpath = "";
+			if(local) { 
+				richFeedbackpath = "./shared/" + richFeedbackFile;
+			} else {
+				richFeedbackpath = "/shared/" + richFeedbackFile;
+			}
+			BufferedWriter richWriter = new BufferedWriter(new FileWriter(richFeedbackpath));
+			String richFeedback = "<html><head><title> Rich Feedback</title></head><body><h1>Rich Feedback</h1><ul><li><font color=green>Green feedback</font></li><li><font color=red>Red feedback</font></li></ul></body></html>";
+			richWriter.write(richFeedback);
+			richWriter.close();
+                } catch(IOException io) {
+                        System.err.println("Got an exception writing to feedback.txt!");
+                        System.err.println(io.getMessage());
+                        io.printStackTrace(System.err);
+                        feedback = io.getMessage();
+                }
+	}
+}
---
+++ custom-graders/v2/DemoJavaGrader/executeGrader.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+# Coursera deletes all environment variables set inside 'Dockerfile'. If any environment varables
+# need to be set, they must be set inside a wrapper bash script.
+
+local=$LOCAL
+echo $local
+
+if [ $local != "1" ]
+then
+	echo "exporting JAVA_HOME"
+	export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+fi
+
+# Switch to the grader directory
+# if [ $local != "1" ]
+# then
+#    cd /grader
+# else
+#    cd ./grader
+# fi
+
+# Use the parsed partId to know which part is being graded in the current run.
+
+if [ $local != "1" ]
+then
+	GRADER_DIRECTORY=FactoringGrader
+else
+	GRADER_DIRECTORY=../FactoringGrader
+fi
+
+SUBMISSION_CLASS=Factoring
+
+
+# Compile the learner's program in the current directory. We can safely assume that there
+# would be a single submission file in this directory.
+if [ $local != "1" ]
+then
+    javac -d . /shared/submission/$filename
+else
+    javac -d . ./shared/submission/$filename
+fi
+
+# Note: Nothing except Json object containing 'fractionalScore' and 'feedback' should be written
+# to stdout.
+
+# Check if the compilation was successful
+if [ ! $? -eq 0 ]; then
+  echo "{ \"fractionalScore\":0.0, \"feedback\":\"Compile Error (GrIDV2 stdout)\" }"
+  echo "{ \"fractionalScore\":0.0, \"feedback\":\"Compile Error (GrIDV2 feedback)\" }" > /shared/feedback.txt
+  exit 0
+fi
+
+# Run the learner's submission with testCases and capture stdout produced in learnerOutput.txt
+cat "$GRADER_DIRECTORY"/testCases.txt | java "$SUBMISSION_CLASS" 1> learnerOutput.txt
+
+# Check if the learner's program ran successfully
+if [ ! $? -eq 0 ]; then
+	echo "{ \"fractionalScore\": 0.0, \"feedback\":\"Your submission produced runtime errors (GrIDV2 stdout)\" }"
+	echo "{ \"fractionalScore\": 0.0, \"feedback\":\"Your submission produced runtime errors (GrIDV2 feedback)\" }" > /shared/feedback.txt
+	exit 0
+fi
+
+# Compile Grader.java
+javac Grader.java
+
+# Use Grader.java to compare learnerOutput.txt and solution.txt
+java Grader "$GRADER_DIRECTORY"/solution.txt learnerOutput.txt
