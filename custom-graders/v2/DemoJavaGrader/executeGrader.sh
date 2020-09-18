024687871d980a7d5597dc4272021196d9cb3a17
Sashi Penta <sashi@coursera.org>
2020-09-18 10:20:38 -0700

    remove LOCAL testing (quite confusing) fixes another issue

--- custom-graders/v2/DemoJavaGrader/executeGrader.sh
+++ custom-graders/v2/DemoJavaGrader/executeGrader.sh
@@ -2,57 +2,27 @@
 
 # Coursera deletes all environment variables set inside 'Dockerfile'. If any environment varables
 # need to be set, they must be set inside a wrapper bash script.
-
-local=$LOCAL
-echo $local
-
-if [ $local != "1" ]
-then
-	echo "exporting JAVA_HOME"
-	export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-fi
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
 # Switch to the grader directory
-# if [ $local != "1" ]
-# then
-#    cd /grader
-# else
-#    cd ./grader
-# fi
+cd /grader
 
 # Use the parsed partId to know which part is being graded in the current run.
 
-if [ $local != "1" ]
-then
-	GRADER_DIRECTORY=/grader/FactoringGrader
-else
-	GRADER_DIRECTORY=./FactoringGrader
-fi
-
+GRADER_DIRECTORY=FactoringGrader
 SUBMISSION_CLASS=Factoring
 
 
 # Compile the learner's program in the current directory. We can safely assume that there
 # would be a single submission file in this directory.
-if [ $local != "1" ]
-then
-    javac -d . /shared/submission/$filename
-else
-    javac -d . ./shared/submission/$filename
-fi
+javac -d . /shared/submission/$filename
 
 # Note: Nothing except Json object containing 'fractionalScore' and 'feedback' should be written
 # to stdout.
 
 # Check if the compilation was successful
 if [ ! $? -eq 0 ]; then
-  echo "{ \"fractionalScore\":0.0, \"feedback\":\"Compile Error (GrIDV2 stdout)\" }"
-  if [ $local != "1" ]
-     then
-         echo "{ \"fractionalScore\":0.0, \"feedback\":\"Compile Error (GrIDV2 feedback)\" }" > /shared/feedback.json
-  else
-         echo "{ \"fractionalScore\":0.0, \"feedback\":\"Compile Error (GrIDV2 feedback)\" }" > ./shared/feedback.json
-  fi
+  echo "{ \"fractionalScore\":0.0, \"feedback\":\"Compile Error\" }" > /shared/feedback.json
   exit 0
 fi
 
@@ -61,18 +31,12 @@ cat "$GRADER_DIRECTORY"/testCases.txt | java "$SUBMISSION_CLASS" 1> learnerOutpu
 
 # Check if the learner's program ran successfully
 if [ ! $? -eq 0 ]; then
-	echo "{ \"fractionalScore\": 0.0, \"feedback\":\"Your submission produced runtime errors (GrIDV2 stdout)\" }"
-	echo "{ \"fractionalScore\": 0.0, \"feedback\":\"Your submission produced runtime errors (GrIDV2 feedback)\" }" > /shared/feedback.json
+	echo "{ \"fractionalScore\": 0.0, \"feedback\":\"Your submission produced runtime errors\" }" > /shared/feedback.json
 	exit 0
 fi
 
 # Compile Grader.java
-if [ $local != "1" ]
-then
-    javac -d . /grader/Grader.java
-else
-    javac -d . ./grader/Grader.java
-fi
+javac Grader.java
 
 # Use Grader.java to compare learnerOutput.txt and solution.txt
 java Grader "$GRADER_DIRECTORY"/solution.txt learnerOutput.txt
