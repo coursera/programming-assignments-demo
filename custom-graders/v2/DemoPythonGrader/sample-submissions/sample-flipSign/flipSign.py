17b2963461e5b1ef87eebcc0aa371ba5b29ff6c1
ji-su-lee <jilee@coursera.org>
2020-11-20 16:09:06 -0800

    adding sample submissions folder for convenience

---
+++ custom-graders/v2/DemoPythonGrader/sample-submissions/sample-flipSign/flipSign.py
@@ -0,0 +1,11 @@
+def flipSign(input):
+    product = input*(-1)
+    return product
+
+# MAIN -------------------------------------------------------------------------
+def main(input):
+    output = flipSign(input)
+    return output
+
+if __name__ == '__main__':
+    main(input)
